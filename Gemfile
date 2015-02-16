087cfb83cb4470ba01cf8f609a67e022d2fef802
Eric Turner <ericturnerdev@gmail.com>
2015-02-16 22:09:13 +0900

    bootstrap-sass挿入、最初のテスト

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 gem 'rails', '4.2.0'
+gem 'bootstrap-sass', '3.2.0.0'
 gem 'pg'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
--- spec/features/user_visits_welcome_page.rb
+++ spec/features/user_visits_welcome_page.rb
@@ -1,6 +1,9 @@
 require "rails_helper"
-RSpec.feature "Navbar" do
-  scenario "contains correct links" do
-
+RSpec.feature "User views navbar links" do
+  scenario "successfully" do
+    visit welcome_path
+    expect(page).to have_content "年号・西暦"
+    expect(page).to have_content "年齢"
+    expect(page).to have_content "干支"
   end
 end
