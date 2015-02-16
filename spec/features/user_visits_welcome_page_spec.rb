9d4103415b1f863074a27febedf3c96535edc5a3
Eric Turner <ericturnerdev@gmail.com>
2015-02-16 23:38:50 +0900

    Welcomeページnavbarテストをグリーンに

--- spec/features/user_visits_welcome_page_spec.rb
+++ spec/features/user_visits_welcome_page_spec.rb
@@ -1,7 +1,15 @@
 require "rails_helper"
+#
+#RSpec.feature "User views navbar" do
+  #scenario "successfully" do
+    #visit root_path
+    #expect(page).to have_css("[data-role=navbar]", text: "nengo")
+  #end
+#end
+
 RSpec.feature "User views navbar links" do
   scenario "successfully" do
-    visit welcome_path
+    visit root_path
     expect(page).to have_content "年号・西暦"
     expect(page).to have_content "年齢"
     expect(page).to have_content "干支"
