41eeef54658a3edd8693c50a012390dfb48c53f0
Eric Turner <ericturnerdev@gmail.com>
2015-02-16 22:54:04 +0900

    最初のテストを赤に

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,18 @@
-
 ENV['RAILS_ENV'] ||= 'test'
+
 require 'spec_helper'
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'capybara/rspec'
+require 'capybara/rails'
+require 'email_spec'
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+ActiveRecord::Migration.maintain_test_schema!
+
+Monban.test_mode!
+
+require File.expand_path('../../config/environment', __FILE__)
 ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -10,7 +20,7 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.include Monban::Test::Helpers, type: feature
-  config.include EmailSpec:Matchers
+  #config.include EmailSpec:Matchers
 
   config.after :each do
     Monban.test_reset!
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,16 @@
 require "factory_girl"
+
 RSpec.configure do |config|
+  config.backtrace_exclusion_patterns = [
+    /gems/,
+  ]
+
+  config.include FactoryGirl::Syntax::Methods
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
-  config.include FactoryGirl::Syntax::Methods
 end
