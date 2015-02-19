bdfb2f858e487abda46fc9072c21e571ac711a51
Eric Turner <ericturnerdev@gmail.com>
2015-02-19 16:17:22 +0000

    祝日、アプリリンク追加

--- spec/features/user_visits_welcome_page_spec.rb
+++ spec/features/user_visits_welcome_page_spec.rb
@@ -1,17 +1,19 @@
 require "rails_helper"
-#
-#RSpec.feature "User views navbar" do
-  #scenario "successfully" do
-    #visit root_path
-    #expect(page).to have_css("[data-role=navbar]", text: "nengo")
-  #end
-#end
+
+RSpec.feature "User views navbar" do
+  scenario "successfully" do
+    visit root_path
+    expect(page).to have_css("[role=navbar]", text: "nengo")
+  end
+end
 
 RSpec.feature "User views navbar links" do
   scenario "successfully" do
     visit root_path
-    expect(page).to have_content "年号・西暦"
+    expect(page).to have_content "年号"
     expect(page).to have_content "年齢"
     expect(page).to have_content "干支"
+    expect(page).to have_content "祝日"
+    expect(page).to have_content "アプリ"
   end
 end
