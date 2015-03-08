08678a52b5222bf0c416ba17d3f31488b4dfc7b2
Eric Turner <ericturnerdev@gmail.com>
2015-03-08 22:38:20 +0900

    祝日ページをアップデートする

--- spec/features/user_visits_shukujitsu_page_spec.rb
+++ spec/features/user_visits_shukujitsu_page_spec.rb
@@ -1,9 +1,20 @@
 require "rails_helper"
 RSpec.feature "User visits 祝日 page" do
   scenario "successfully" do
+    visit_holidays_page
+    expect(page).to have_content "祝日ページ"
+  end
+
+  scenario "and views holidays successfully" do
+    visit_holidays_page
+    expect(page).to have_css("table[data-role=holidays]")
+    expect(page).to have_content("元日")
+    expect(page).to have_content("天皇誕生日")
+  end
+
+  def visit_holidays_page
     visit "/"
     click_link "祝日"
-    expect(page).to have_content "祝日ページ"
   end
 end
 
