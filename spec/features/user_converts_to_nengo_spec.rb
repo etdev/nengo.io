b8fb42fe01e8eff88ea1630148be0e1bef93c668
Eric Turner <ericturnerdev@gmail.com>
2015-03-09 11:47:54 +0000

    年号ページのテストを追加

---
+++ spec/features/user_converts_to_nengo_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+RSpec.feature "User converts seireki year to nengo" do
+  scenario "successfully" do
+    visit root_path
+    fill_in "西暦の年", with: "2015"
+    click_button "変換する"
+    expect(page).to have_content "平成"
+    expect(page).to have_content "27"
+  end
+end
