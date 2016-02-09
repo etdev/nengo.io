7c59a02a8cc151bc5c62b52f2f7d03ae17ebc979
Eric Turner <ericturnerdev@gmail.com>
2016-02-09 12:24:34 +0000

    Add app screenshots, update holidays

--- spec/features/user_converts_to_nengo_spec.rb
+++ spec/features/user_converts_to_nengo_spec.rb
@@ -2,7 +2,6 @@ require "rails_helper"
 RSpec.feature "User converts seireki year to nengo" do
   scenario "successfully" do
     visit root_path
-    binding.pry
     fill_in "西暦の年", with: "2014"
     page.find("#submit_seireki").click
     #expect(page).to have_content "平成"
